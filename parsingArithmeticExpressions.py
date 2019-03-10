f9e96d61b9697771e63753809df7657c438f2543
Scott Wolfe <sawolfe1@gmail.com>
2019-03-10 16:14:11 -0500

    added calculations and error handling for division by 0

--- parsingArithmeticExpressions.py
+++ parsingArithmeticExpressions.py
@@ -1,39 +1,73 @@
 
-
-expression = input('Enter an arithmetic expression as a string: ')
-Calculate(expression)
-
 def Calculate(expression):
+    postfix = ConvertoToPostfix(expression).split(',')
+    stack = []
+    operators = ["+", "-", "*", "/"]
 
+    for i in postfix:
+        if i.isdigit():
+            stack.append(i)
+        elif i in operators:
+            try:
+                stack.append(DoMath(i, stack))
+            except:
+                print("Error: Divison by 0")
+                return
+                
+    print(stack.pop())
+
+def DoMath(operator, stack):
+    right = int(stack.pop())
+    left = int(stack.pop())
+
+    if operator == "+":
+        return left+right
+    elif operator == "-":
+        return left-right
+    elif operator == "*":
+        return left*right
+    elif operator == "/":
+        return left//right
+
+def ConvertoToPostfix(expression):
     stack = []
     buffer = ""
-    operators = ["+", "-", "/", "*"]
-    for i in expression:
-        if i.isdigit():
-            buffer += f"{i}"
-        elif i == "(":
-            stack.append(i)
-        elif i == ")":
-            while stack and stack[-1] != "(":
+    length = len(expression)
+
+    for i, char in enumerate(expression):
+        if char.isdigit():
+            buffer += f"{char}"
+            j=i+1
+            if j<length and expression[j].isdigit():
+                continue
+            buffer += ","
+        elif char == "(":
+            stack.append(char)
+        elif char == ")":
+            while stack:
                 val = stack.pop()
-                buffer += f"{val}"
+                if val == "(":
+                    break
+                buffer += f"{val},"
         else:
-            while stack and GetPrecedence(i, stack):
-                buffer += f"{stack.pop()}"
-            stack.append(i)
+            while stack and GetPrecedence(char, stack[-1]):
+                buffer += f"{stack.pop()},"
+            stack.append(char)
 
     while stack:
-        buffer += f"{stack.pop()}"
+        buffer += f"{stack.pop()},"
         
-    print(buffer)
+    return(buffer.strip(','))
 
 
-def GetPrecedence(val, stack):
-    precedence = {'+':1, '-':1, '*':2, '/':2}
+def GetPrecedence(val, topOfStack):
+    precedence = {"+" : 0, "-" : 0, "*" : 1, "/" : 1}
 
     try:
-        a = precedence[val]
-        b = precedence[stack[-1]]
-        return a<=b
+        return precedence[val]<=precedence[topOfStack]
     except KeyError:
         return False
+
+while True:
+    expression = input('Enter an arithmetic expression as a string: ')
+    Calculate(expression)
